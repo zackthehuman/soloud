65c3ab3a27d35844949b1a04eb4a51f9a36170ee
Zack Mulgrew <zack@zackthehuman.com>
2024-04-07 14:31:37 -0700

    use unique variable names

--- cmake/demos.cmake
+++ cmake/demos.cmake
@@ -1,5 +1,5 @@
-set (HEADER_PATH ../include)
-set (SOURCE_PATH ../demos)
+set (SOLOUD_DEMOS_HEADER_PATH ../include)
+set (SOLOUD_DEMOS_SOURCE_PATH ../demos)
 
 function (soloud_add_demo name sources)
 	set (TARGET_NAME SoLoud_${name})
@@ -8,31 +8,31 @@ function (soloud_add_demo name sources)
 	include (Install)
 endfunction()
 
-include_directories (${SOURCE_PATH}/common)
-include_directories (${SOURCE_PATH}/common/imgui)
+include_directories (${SOLOUD_DEMOS_SOURCE_PATH}/common)
+include_directories (${SOLOUD_DEMOS_SOURCE_PATH}/common/imgui)
 
 
-# soloud_add_demo(c_test ${SOURCE_PATH}/c_test/main.c)
-soloud_add_demo(enumerate ${SOURCE_PATH}/enumerate/main.cpp)
-# soloud_add_demo(env ${SOURCE_PATH}/env/main.cpp)
+# soloud_add_demo(c_test ${SOLOUD_DEMOS_SOURCE_PATH}/c_test/main.c)
+soloud_add_demo(enumerate ${SOLOUD_DEMOS_SOURCE_PATH}/enumerate/main.cpp)
+# soloud_add_demo(env ${SOLOUD_DEMOS_SOURCE_PATH}/env/main.cpp)
 # soloud_add_demo(megademo
-# 	${SOURCE_PATH}/megademo/3dtest.cpp
-# 	${SOURCE_PATH}/megademo/main.cpp
-# 	${SOURCE_PATH}/megademo/mixbusses.cpp
-# 	${SOURCE_PATH}/megademo/monotone.cpp
-# 	${SOURCE_PATH}/megademo/multimusic.cpp
-# 	${SOURCE_PATH}/megademo/pewpew.cpp
-# 	${SOURCE_PATH}/megademo/radiogaga.cpp
-# 	${SOURCE_PATH}/megademo/space.cpp
-# 	${SOURCE_PATH}/megademo/speechfilter.cpp
-# 	${SOURCE_PATH}/megademo/tedsid.cpp
-# 	${SOURCE_PATH}/megademo/virtualvoices.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/3dtest.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/main.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/mixbusses.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/monotone.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/multimusic.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/pewpew.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/radiogaga.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/space.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/speechfilter.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/tedsid.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/megademo/virtualvoices.cpp
 # )
-soloud_add_demo(null ${SOURCE_PATH}/null/main.cpp)
+soloud_add_demo(null ${SOLOUD_DEMOS_SOURCE_PATH}/null/main.cpp)
 # soloud_add_demo(piano
-# 	${SOURCE_PATH}/piano/main.cpp
-# 	${SOURCE_PATH}/piano/soloud_basicwave.cpp
-# 	${SOURCE_PATH}/piano/soloud_padsynth.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/piano/main.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/piano/soloud_basicwave.cpp
+# 	${SOLOUD_DEMOS_SOURCE_PATH}/piano/soloud_padsynth.cpp
 # )
-soloud_add_demo(simplest ${SOURCE_PATH}/simplest/main.cpp)
-soloud_add_demo(welcome ${SOURCE_PATH}/welcome/main.cpp)
+soloud_add_demo(simplest ${SOLOUD_DEMOS_SOURCE_PATH}/simplest/main.cpp)
+soloud_add_demo(welcome ${SOLOUD_DEMOS_SOURCE_PATH}/welcome/main.cpp)
--- cmake/src.cmake
+++ cmake/src.cmake
@@ -1,149 +1,149 @@
-set (TARGET_NAME soloud)
+set (SOLOUD_TARGET_NAME soloud)
 
-set (HEADER_PATH ../include)
-set (SOURCE_PATH ../src)
+set (SOLOUD_HEADER_PATH ../include)
+set (SOLOUD_SOURCE_PATH ../src)
 
-set (LINK_LIBRARIES)
+set (SOLOUD_LINK_LIBRARIES)
 
 # Headers
-set (TARGET_HEADERS
-	${HEADER_PATH}/soloud.h
-	${HEADER_PATH}/soloud_audiosource.h
-	${HEADER_PATH}/soloud_ay.h
-	${HEADER_PATH}/soloud_bassboostfilter.h
-	${HEADER_PATH}/soloud_biquadresonantfilter.h
-	${HEADER_PATH}/soloud_bus.h
-	${HEADER_PATH}/soloud_dcremovalfilter.h
-	${HEADER_PATH}/soloud_echofilter.h
-	${HEADER_PATH}/soloud_error.h
-	${HEADER_PATH}/soloud_fader.h
-	${HEADER_PATH}/soloud_fft.h
-	${HEADER_PATH}/soloud_fftfilter.h
-	${HEADER_PATH}/soloud_file.h
-	${HEADER_PATH}/soloud_file_hack_off.h
-	${HEADER_PATH}/soloud_file_hack_on.h
-	${HEADER_PATH}/soloud_filter.h
-	${HEADER_PATH}/soloud_flangerfilter.h
-	${HEADER_PATH}/soloud_freeverbfilter.h
-	${HEADER_PATH}/soloud_internal.h
-	${HEADER_PATH}/soloud_lofifilter.h
-	${HEADER_PATH}/soloud_misc.h
-	${HEADER_PATH}/soloud_monotone.h
-	${HEADER_PATH}/soloud_noise.h
-	${HEADER_PATH}/soloud_openmpt.h
-	${HEADER_PATH}/soloud_queue.h
-	${HEADER_PATH}/soloud_robotizefilter.h
-	${HEADER_PATH}/soloud_sfxr.h
-	${HEADER_PATH}/soloud_speech.h
-	${HEADER_PATH}/soloud_tedsid.h
-	${HEADER_PATH}/soloud_thread.h
-	${HEADER_PATH}/soloud_vic.h
-	${HEADER_PATH}/soloud_vizsn.h
-	${HEADER_PATH}/soloud_wav.h
-	${HEADER_PATH}/soloud_waveshaperfilter.h
-	${HEADER_PATH}/soloud_wavstream.h
+set (SOLOUD_TARGET_HEADERS
+	${SOLOUD_HEADER_PATH}/soloud.h
+	${SOLOUD_HEADER_PATH}/soloud_audiosource.h
+	${SOLOUD_HEADER_PATH}/soloud_ay.h
+	${SOLOUD_HEADER_PATH}/soloud_bassboostfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_biquadresonantfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_bus.h
+	${SOLOUD_HEADER_PATH}/soloud_dcremovalfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_echofilter.h
+	${SOLOUD_HEADER_PATH}/soloud_error.h
+	${SOLOUD_HEADER_PATH}/soloud_fader.h
+	${SOLOUD_HEADER_PATH}/soloud_fft.h
+	${SOLOUD_HEADER_PATH}/soloud_fftfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_file.h
+	${SOLOUD_HEADER_PATH}/soloud_file_hack_off.h
+	${SOLOUD_HEADER_PATH}/soloud_file_hack_on.h
+	${SOLOUD_HEADER_PATH}/soloud_filter.h
+	${SOLOUD_HEADER_PATH}/soloud_flangerfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_freeverbfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_internal.h
+	${SOLOUD_HEADER_PATH}/soloud_lofifilter.h
+	${SOLOUD_HEADER_PATH}/soloud_misc.h
+	${SOLOUD_HEADER_PATH}/soloud_monotone.h
+	${SOLOUD_HEADER_PATH}/soloud_noise.h
+	${SOLOUD_HEADER_PATH}/soloud_openmpt.h
+	${SOLOUD_HEADER_PATH}/soloud_queue.h
+	${SOLOUD_HEADER_PATH}/soloud_robotizefilter.h
+	${SOLOUD_HEADER_PATH}/soloud_sfxr.h
+	${SOLOUD_HEADER_PATH}/soloud_speech.h
+	${SOLOUD_HEADER_PATH}/soloud_tedsid.h
+	${SOLOUD_HEADER_PATH}/soloud_thread.h
+	${SOLOUD_HEADER_PATH}/soloud_vic.h
+	${SOLOUD_HEADER_PATH}/soloud_vizsn.h
+	${SOLOUD_HEADER_PATH}/soloud_wav.h
+	${SOLOUD_HEADER_PATH}/soloud_waveshaperfilter.h
+	${SOLOUD_HEADER_PATH}/soloud_wavstream.h
 )
 
 
 # Core
-set (CORE_PATH ${SOURCE_PATH}/core)
-set (CORE_SOURCES
-	${CORE_PATH}/soloud.cpp
-	${CORE_PATH}/soloud_audiosource.cpp
-	${CORE_PATH}/soloud_bus.cpp
-	${CORE_PATH}/soloud_core_3d.cpp
-	${CORE_PATH}/soloud_core_basicops.cpp
-	${CORE_PATH}/soloud_core_faderops.cpp
-	${CORE_PATH}/soloud_core_filterops.cpp
-	${CORE_PATH}/soloud_core_getters.cpp
-	${CORE_PATH}/soloud_core_setters.cpp
-	${CORE_PATH}/soloud_core_voicegroup.cpp
-	${CORE_PATH}/soloud_core_voiceops.cpp
-	${CORE_PATH}/soloud_fader.cpp
-	${CORE_PATH}/soloud_fft.cpp
-	${CORE_PATH}/soloud_fft_lut.cpp
-	${CORE_PATH}/soloud_file.cpp
-	${CORE_PATH}/soloud_filter.cpp
-	${CORE_PATH}/soloud_misc.cpp
-	${CORE_PATH}/soloud_queue.cpp
-	${CORE_PATH}/soloud_thread.cpp
+set (SOLOUD_CORE_PATH ${SOLOUD_SOURCE_PATH}/core)
+set (SOLOUD_CORE_SOURCES
+	${SOLOUD_CORE_PATH}/soloud.cpp
+	${SOLOUD_CORE_PATH}/soloud_audiosource.cpp
+	${SOLOUD_CORE_PATH}/soloud_bus.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_3d.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_basicops.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_faderops.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_filterops.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_getters.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_setters.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_voicegroup.cpp
+	${SOLOUD_CORE_PATH}/soloud_core_voiceops.cpp
+	${SOLOUD_CORE_PATH}/soloud_fader.cpp
+	${SOLOUD_CORE_PATH}/soloud_fft.cpp
+	${SOLOUD_CORE_PATH}/soloud_fft_lut.cpp
+	${SOLOUD_CORE_PATH}/soloud_file.cpp
+	${SOLOUD_CORE_PATH}/soloud_filter.cpp
+	${SOLOUD_CORE_PATH}/soloud_misc.cpp
+	${SOLOUD_CORE_PATH}/soloud_queue.cpp
+	${SOLOUD_CORE_PATH}/soloud_thread.cpp
 )
 
 
 # Audiosources
-set (AUDIOSOURCES_PATH ${SOURCE_PATH}/audiosource)
-set (AUDIOSOURCES_SOURCES
+set (SOLOUD_AUDIOSOURCES_PATH ${SOLOUD_SOURCE_PATH}/audiosource)
+set (SOLOUD_AUDIOSOURCES_SOURCES
 	# ay
-	${AUDIOSOURCES_PATH}/ay/chipplayer.cpp
-	${AUDIOSOURCES_PATH}/ay/chipplayer.h
-	${AUDIOSOURCES_PATH}/ay/readme.txt
-	${AUDIOSOURCES_PATH}/ay/sndbuffer.cpp
-	${AUDIOSOURCES_PATH}/ay/sndbuffer.h
-	${AUDIOSOURCES_PATH}/ay/sndchip.cpp
-	${AUDIOSOURCES_PATH}/ay/sndchip.h
-	${AUDIOSOURCES_PATH}/ay/sndrender.cpp
-	${AUDIOSOURCES_PATH}/ay/sndrender.h
-	${AUDIOSOURCES_PATH}/ay/soloud_ay.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/chipplayer.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/chipplayer.h
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/readme.txt
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndbuffer.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndbuffer.h
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndchip.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndchip.h
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndrender.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/sndrender.h
+	${SOLOUD_AUDIOSOURCES_PATH}/ay/soloud_ay.cpp
 
 	# monotone
-	${AUDIOSOURCES_PATH}/monotone/soloud_monotone.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/monotone/soloud_monotone.cpp
 
 	# noise
-	${AUDIOSOURCES_PATH}/noise/soloud_noise.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/noise/soloud_noise.cpp
 
 	# openmpt
-	${AUDIOSOURCES_PATH}/openmpt/soloud_openmpt.cpp
-	${AUDIOSOURCES_PATH}/openmpt/soloud_openmpt_dll.c
+	${SOLOUD_AUDIOSOURCES_PATH}/openmpt/soloud_openmpt.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/openmpt/soloud_openmpt_dll.c
 
 	# sfxr
-	${AUDIOSOURCES_PATH}/sfxr/soloud_sfxr.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/sfxr/soloud_sfxr.cpp
 
 	# speech
-	${AUDIOSOURCES_PATH}/speech/Elements.def
-	${AUDIOSOURCES_PATH}/speech/darray.cpp
-	${AUDIOSOURCES_PATH}/speech/darray.h
-	${AUDIOSOURCES_PATH}/speech/klatt.cpp
-	${AUDIOSOURCES_PATH}/speech/klatt.h
-	${AUDIOSOURCES_PATH}/speech/resonator.cpp
-	${AUDIOSOURCES_PATH}/speech/resonator.h
-	${AUDIOSOURCES_PATH}/speech/soloud_speech.cpp
-	${AUDIOSOURCES_PATH}/speech/tts.cpp
-	${AUDIOSOURCES_PATH}/speech/tts.h
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/Elements.def
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/darray.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/darray.h
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/klatt.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/klatt.h
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/resonator.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/resonator.h
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/soloud_speech.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/tts.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/speech/tts.h
 
 	# tedsid
-	${AUDIOSOURCES_PATH}/tedsid/sid.cpp
-	${AUDIOSOURCES_PATH}/tedsid/sid.h
-	${AUDIOSOURCES_PATH}/tedsid/soloud_tedsid.cpp
-	${AUDIOSOURCES_PATH}/tedsid/ted.cpp
-	${AUDIOSOURCES_PATH}/tedsid/ted.h
+	${SOLOUD_AUDIOSOURCES_PATH}/tedsid/sid.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/tedsid/sid.h
+	${SOLOUD_AUDIOSOURCES_PATH}/tedsid/soloud_tedsid.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/tedsid/ted.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/tedsid/ted.h
 
 	# vic
-	${AUDIOSOURCES_PATH}/vic/soloud_vic.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/vic/soloud_vic.cpp
 
 	# vizsn
-	${AUDIOSOURCES_PATH}/vizsn/soloud_vizsn.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/vizsn/soloud_vizsn.cpp
 
 	# wav
-	${AUDIOSOURCES_PATH}/wav/dr_flac.h
-	${AUDIOSOURCES_PATH}/wav/dr_impl.cpp
-	${AUDIOSOURCES_PATH}/wav/dr_mp3.h
-	${AUDIOSOURCES_PATH}/wav/dr_wav.h
-	${AUDIOSOURCES_PATH}/wav/soloud_wav.cpp
-	${AUDIOSOURCES_PATH}/wav/soloud_wavstream.cpp
-	${AUDIOSOURCES_PATH}/wav/stb_vorbis.c
-	${AUDIOSOURCES_PATH}/wav/stb_vorbis.h
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/dr_flac.h
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/dr_impl.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/dr_mp3.h
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/dr_wav.h
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/soloud_wav.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/soloud_wavstream.cpp
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/stb_vorbis.c
+	${SOLOUD_AUDIOSOURCES_PATH}/wav/stb_vorbis.h
 )
 
 
 # Backends
 # TODO: Other backends
-set (BACKENDS_PATH ${SOURCE_PATH}/backend)
-set (BACKENDS_SOURCES)
+set (SOLOUD_BACKENDS_PATH ${SOLOUD_SOURCE_PATH}/backend)
+set (SOLOUD_BACKENDS_SOURCES)
 
 if (SOLOUD_BACKEND_NULL)
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/null/soloud_null.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/null/soloud_null.cpp
 	)
 	add_definitions(-DWITH_NULL)
 endif()
@@ -153,13 +153,13 @@ if (SOLOUD_BACKEND_SDL2)
 	include_directories (${SDL2_INCLUDE_DIR})
 	add_definitions (-DWITH_SDL2_STATIC)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/sdl2_static/soloud_sdl2_static.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/sdl2_static/soloud_sdl2_static.cpp
 	)
 
-	set (LINK_LIBRARIES
-		${LINK_LIBRARIES}
+	set (SOLOUD_LINK_LIBRARIES
+		${SOLOUD_LINK_LIBRARIES}
 		${SDL2_LIBRARY}
 	)
 
@@ -168,14 +168,14 @@ endif()
 if (SOLOUD_BACKEND_ALSA)                     
     add_definitions (-DWITH_ALSA)                
                                            
-    set (BACKENDS_SOURCES              
-        ${BACKENDS_SOURCES} 
-        ${BACKENDS_PATH}/alsa/soloud_alsa.cpp
+    set (SOLOUD_BACKENDS_SOURCES              
+        ${SOLOUD_BACKENDS_SOURCES} 
+        ${SOLOUD_BACKENDS_PATH}/alsa/soloud_alsa.cpp
     )                                              
 
     find_library (ALSA_LIBRARY asound)
-    set (LINK_LIBRARIES
-        ${LINK_LIBRARIES}
+    set (SOLOUD_LINK_LIBRARIES
+        ${SOLOUD_LINK_LIBRARIES}
         ${ALSA_LIBRARY}
     )
 endif()
@@ -188,14 +188,14 @@ if (SOLOUD_BACKEND_COREAUDIO)
 
 	add_definitions (-DWITH_COREAUDIO)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/coreaudio/soloud_coreaudio.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/coreaudio/soloud_coreaudio.cpp
 	)
 
 	find_library (AUDIOTOOLBOX_FRAMEWORK AudioToolbox)
-	set (LINK_LIBRARIES
-		${LINK_LIBRARIES}
+	set (SOLOUD_LINK_LIBRARIES
+		${SOLOUD_LINK_LIBRARIES}
 		${AUDIOTOOLBOX_FRAMEWORK}
 	)
 endif()
@@ -204,14 +204,14 @@ endif()
 if (SOLOUD_BACKEND_OPENSLES)
 	add_definitions (-DWITH_OPENSLES)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/opensles/soloud_opensles.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/opensles/soloud_opensles.cpp
 	)
 
 	find_library (OPENSLES_LIBRARY OpenSLES)
-	set (LINK_LIBRARIES
-		${LINK_LIBRARIES}
+	set (SOLOUD_LINK_LIBRARIES
+		${SOLOUD_LINK_LIBRARIES}
 		${OPENSLES_LIBRARY}
 	)
 endif()
@@ -220,81 +220,81 @@ endif()
 if (SOLOUD_BACKEND_XAUDIO2)
 	add_definitions (-DWITH_XAUDIO2)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/xaudio2/soloud_xaudio2.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/xaudio2/soloud_xaudio2.cpp
 	)
 endif()
 
 if (SOLOUD_BACKEND_WINMM)
 	add_definitions (-DWITH_WINMM)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/winmm/soloud_winmm.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/winmm/soloud_winmm.cpp
 	)
 endif()
 
 if (SOLOUD_BACKEND_WASAPI)
 	add_definitions (-DWITH_WASAPI)
 
-	set (BACKENDS_SOURCES
-		${BACKENDS_SOURCES}
-		${BACKENDS_PATH}/wasapi/soloud_wasapi.cpp
+	set (SOLOUD_BACKENDS_SOURCES
+		${SOLOUD_BACKENDS_SOURCES}
+		${SOLOUD_BACKENDS_PATH}/wasapi/soloud_wasapi.cpp
 	)
 endif()
 
 # Filters
-set (FILTERS_PATH ${SOURCE_PATH}/filter)
-set (FILTERS_SOURCES
-	${FILTERS_PATH}/soloud_bassboostfilter.cpp
-	${FILTERS_PATH}/soloud_biquadresonantfilter.cpp
-	${FILTERS_PATH}/soloud_dcremovalfilter.cpp
-	${FILTERS_PATH}/soloud_echofilter.cpp
-	${FILTERS_PATH}/soloud_fftfilter.cpp
-	${FILTERS_PATH}/soloud_flangerfilter.cpp
-	${FILTERS_PATH}/soloud_freeverbfilter.cpp
-	${FILTERS_PATH}/soloud_lofifilter.cpp
-	${FILTERS_PATH}/soloud_robotizefilter.cpp
-	${FILTERS_PATH}/soloud_waveshaperfilter.cpp
+set (SOLOUD_FILTERS_PATH ${SOLOUD_SOURCE_PATH}/filter)
+set (SOLOUD_FILTERS_SOURCES
+	${SOLOUD_FILTERS_PATH}/soloud_bassboostfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_biquadresonantfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_dcremovalfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_echofilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_fftfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_flangerfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_freeverbfilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_lofifilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_robotizefilter.cpp
+	${SOLOUD_FILTERS_PATH}/soloud_waveshaperfilter.cpp
 )
 
 
 # All together
-source_group ("Includes"		FILES ${TARGET_HEADERS})
-source_group ("Core"			FILES ${CORE_SOURCES})
-source_group ("Audiosources"	FILES ${AUDIOSOURCES_SOURCES})
-source_group ("Backends"		FILES ${BACKENDS_SOURCES})
-source_group ("Filters"			FILES ${FILTERS_SOURCES})
+source_group ("SoLoud_Includes"     FILES ${SOLOUD_TARGET_HEADERS})
+source_group ("SoLoud_Core"         FILES ${SOLOUD_CORE_SOURCES})
+source_group ("SoLoud_Audiosources" FILES ${SOLOUD_AUDIOSOURCES_SOURCES})
+source_group ("SoLoud_Backends"     FILES ${SOLOUD_BACKENDS_SOURCES})
+source_group ("SoLoud_Filters"      FILES ${SOLOUD_FILTERS_SOURCES})
 
-set (TARGET_SOURCES
-	${CORE_SOURCES}
-	${AUDIOSOURCES_SOURCES}
-	${BACKENDS_SOURCES}
-	${FILTERS_SOURCES}
+set (SOLOUD_TARGET_SOURCES
+	${SOLOUD_CORE_SOURCES}
+	${SOLOUD_AUDIOSOURCES_SOURCES}
+	${SOLOUD_BACKENDS_SOURCES}
+	${SOLOUD_FILTERS_SOURCES}
 )
 
 if (SOLOUD_C_API)
-	set (TARGET_SOURCES
-		${TARGET_SOURCES}
-		${SOURCE_PATH}/c_api/soloud.def
-		${SOURCE_PATH}/c_api/soloud_c.cpp
+	set (SOLOUD_TARGET_SOURCES
+		${SOLOUD_TARGET_SOURCES}
+		${SOLOUD_SOURCE_PATH}/c_api/soloud.def
+		${SOLOUD_SOURCE_PATH}/c_api/soloud_c.cpp
 	)
-	set (TARGET_HEADERS
-		${TARGET_HEADERS}
-		${HEADER_PATH}/soloud_c.h
+	set (SOLOUD_TARGET_HEADERS
+		${SOLOUD_TARGET_HEADERS}
+		${SOLOUD_HEADER_PATH}/soloud_c.h
 	)
 endif()
 
 if (SOLOUD_DYNAMIC)
-	add_library(${TARGET_NAME} ${TARGET_SOURCES})
+	add_library(${SOLOUD_TARGET_NAME} ${SOLOUD_TARGET_SOURCES})
 endif ()
 
 if (SOLOUD_STATIC)
-	add_library(${TARGET_NAME} STATIC ${TARGET_SOURCES})
+	add_library(${SOLOUD_TARGET_NAME} STATIC ${SOLOUD_TARGET_SOURCES})
 endif()
 
-target_link_libraries (${TARGET_NAME} ${LINK_LIBRARIES})
+target_link_libraries (${SOLOUD_TARGET_NAME} ${SOLOUD_LINK_LIBRARIES})
 
 include (Install)
-INSTALL(FILES ${TARGET_HEADERS} DESTINATION include/${TARGET_NAME})
+INSTALL(FILES ${SOLOUD_TARGET_HEADERS} DESTINATION include/${SOLOUD_TARGET_NAME})
